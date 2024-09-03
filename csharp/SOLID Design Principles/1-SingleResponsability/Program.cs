c4d340592be032d631bf5cdb49ce226ab237d764
Irving Bautista <ibautista@tigerconnect.com>
2024-09-03 17:16:29 -0600

    Refactor to adhere to Single Responsibility Principle

--- csharp/SOLID Design Principles/1-SingleResponsability/Program.cs
+++ csharp/SOLID Design Principles/1-SingleResponsability/Program.cs
@@ -1,7 +1,8 @@
 ﻿using SingleResponsability;
 
 StudentRepository studentRepository = new();
-studentRepository.Export();
+ExportServiceCSV exportServiceCSV = new();
+exportServiceCSV.ExportStudents(studentRepository.GetAll());
 Console.WriteLine("Process completed!");
 
 Console.WriteLine("Press any key to finish...");
