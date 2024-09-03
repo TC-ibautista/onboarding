855f13d4e489d09eab05e672f4dfd6eb19d9b30e
Irving Bautista <ibautista@tigerconnect.com>
2024-09-03 12:54:47 -0600

    Implement clean code in Program.cs

--- csharp/Clean Code/ToDo/Program.cs
+++ csharp/Clean Code/ToDo/Program.cs
@@ -5,33 +5,48 @@ namespace ToDo
 {
     internal class Program
     {
-        public static List<string> TL { get; set; }
+        public static List<string> TaskList { get; set; } = new List<string>();
 
         static void Main(string[] args)
         {
-            TL = new List<string>();
-            int variable = 0;
+            const int addTask = 1;
+            const int removeTask = 2; 
+            const int consultTasks = 3; 
+            const int exit = 4;
+            int menuOption = 0;
+
             do
             {
-                variable = ShowMainMenu();
-                if (variable == 1)
+                menuOption = ShowMainMenu();
+                switch (menuOption)
                 {
-                    ShowMenuAdd();
+                    case addTask:
+                        ShowMenuAdd();
+                        break;
+
+                    case removeTask:
+                        ShowMenuRemove();
+                        break;
+
+                    case consultTasks:
+                        ShowMenuConsult();
+                        break;
+
+                    case exit:
+                        Console.WriteLine("See you!");
+                        break;
+
+                    default:
+                        Console.WriteLine("Option not allowed, try again");
+                        break;
                 }
-                else if (variable == 2)
-                {
-                    ShowMenuDos();
-                }
-                else if (variable == 3)
-                {
-                    ShowMenuTres();
-                }
-            } while (variable != 4);
+            } while (menuOption != exit);
         }
+        
         /// <summary>
-        /// Show the main menu 
+        /// Shows the tasks options to redirect to the selected menu
         /// </summary>
-        /// <returns>Returns option indicated by user</returns>
+        /// <returns> Returns option selected by user </returns>
         public static int ShowMainMenu()
         {
             Console.WriteLine("----------------------------------------");
@@ -41,70 +56,93 @@ namespace ToDo
             Console.WriteLine("3. Pending tasks");
             Console.WriteLine("4. Exit");
 
-            // Read line
-            string line = Console.ReadLine();
-            return Convert.ToInt32(line);
+            string optionSelected = Console.ReadLine();
+            return Convert.ToInt32(optionSelected);
         }
 
-        public static void ShowMenuDos()
+        /// <summary>
+        /// Allows the user to remove one task from the list
+        /// </summary>
+        /// <returns> void </returns>
+        public static void ShowMenuRemove()
         {
             try
             {
-                Console.WriteLine("Enter the number of the task to remove: ");
-                // Show current taks
-                for (int i = 0; i < TL.Count; i++)
+                if (TaskList.Count == 0) 
                 {
-                    Console.WriteLine((i + 1) + ". " + TL[i]);
+                    Console.WriteLine("There are no tasks to delete");
+                    return;
                 }
-                Console.WriteLine("----------------------------------------");
 
-                string line = Console.ReadLine();
-                // Remove one position
-                int indexToRemove = Convert.ToInt32(line) - 1;
-                if (indexToRemove > -1)
+                Console.WriteLine("Enter the number of the task to remove: ");
+                ShowCurrentTasks();
+                string taskToRemove = Console.ReadLine();
+                int indexToRemove = Convert.ToInt32(taskToRemove) - 1;
+
+                if (indexToRemove > (TaskList.Count - 1) || indexToRemove < 0)
                 {
-                    if (TL.Count > 0)
-                    {
-                        string task = TL[indexToRemove];
-                        TL.RemoveAt(indexToRemove);
-                        Console.WriteLine("Task " + task + " deleted");
-                    }
+                    Console.WriteLine("Number of task not allowed");
+                    return;
+                }
+                
+                if (indexToRemove > -1 && TaskList.Count > 0)
+                {
+                    string taskRemoved = TaskList[indexToRemove];
+                    TaskList.RemoveAt(indexToRemove);
+                    Console.WriteLine("Task " + taskRemoved + " deleted");
                 }
             }
             catch (Exception)
             {
+                Console.WriteLine("An error occurred while deleting the task");
             }
         }
 
+        /// <summary>
+        /// Allows the user to add one task to the list
+        /// </summary>
+        /// <returns> void </returns>
         public static void ShowMenuAdd()
         {
             try
             {
                 Console.WriteLine("Enter the name of the task: ");
-                string task = Console.ReadLine();
-                TL.Add(task);
+                string newTask = Console.ReadLine();
+                TaskList.Add(newTask);
                 Console.WriteLine("Task registered successfully");
             }
             catch (Exception)
             {
+                Console.WriteLine("An error occurred while adding the task");
             }
         }
 
-        public static void ShowMenuTres()
+        /// <summary>
+        /// Verify if there are tasks in the list and then show them
+        /// </summary>
+        /// <returns> void </returns>
+        public static void ShowMenuConsult()
         {
-            if (TL == null || TL.Count == 0)
+            if (TaskList == null || TaskList.Count == 0)
             {
                 Console.WriteLine("There are no tasks to perform");
             } 
             else
             {
-                Console.WriteLine("----------------------------------------");
-                for (int i = 0; i < TL.Count; i++)
-                {
-                    Console.WriteLine((i + 1) + ". " + TL[i]);
-                }
-                Console.WriteLine("----------------------------------------");
+                ShowCurrentTasks();
             }
+        }
+
+        /// <summary>
+        /// Show all the task of the list
+        /// </summary>
+        /// <returns> void </returns>
+        public static void ShowCurrentTasks()
+        {
+            Console.WriteLine("----------------------------------------");
+            var indexTask = 1;
+            TaskList.ForEach(task => Console.WriteLine((indexTask++) + ". " + task));
+            Console.WriteLine("----------------------------------------");
         }
     }
 }
