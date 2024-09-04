719207bd65e53e671cd7da644c142cabe956782b
Irving Bautista <ibautista@tigerconnect.com>
2024-09-04 11:37:38 -0600

    Refactor to adhere to Interface Segregation Principle

---
+++ csharp/SOLID Design Principles/4-InterfaceSegregation/ICoordinateActivities.cs
@@ -0,0 +1,8 @@
+namespace InterfaceSegregation
+{
+    public interface ICoordinateActivities
+    {
+        void Plan();
+        void Comunicate();
+    }
+}
--- csharp/SOLID Design Principles/4-InterfaceSegregation/Program.cs
+++ csharp/SOLID Design Principles/4-InterfaceSegregation/Program.cs
@@ -1,6 +1,8 @@
 ﻿using InterfaceSegregation;
 
+new ScrumMaster().Comunicate();
 new Developer().Develop();
+new Tester().Test();
 
 Console.WriteLine("Press any key to finish...");
 Console.ReadKey();
